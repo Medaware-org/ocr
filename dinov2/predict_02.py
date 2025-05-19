29f874a325335af425f16cf6cdfb2dfa99071e3e
Clarissa <clarissa3.haas@chello.at>
2025-05-19 13:54:50 +0200

    added umit endpoint tests and small adjustments

--- dinov2/predict_02.py
+++ dinov2/predict_02.py
@@ -14,11 +14,16 @@ import matplotlib
 
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+from pathlib import Path
+
+# Always resolve from the location of this file
+BASE_DIR = Path(__file__).resolve().parent
 
 # --- Step 1: Load the processor and model ---
 processor = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
 model = AutoModel.from_pretrained("facebook/dinov2-base")
 model.eval()
+
 
 # --- Step 2: Function to load and process an image ---
 def process_image(image: Image.Image | np.ndarray):
@@ -50,6 +55,7 @@ class PredictMode(Enum):
     NORMAL = 1  # Compare with computer images
     CANNY = 2  # Compare with computer images with canny filter
     PHOTO = 3  # Compare with photo images
+    ROTATION180 = 4  # Compare with computer images 180 rotated
 
 
 def show_result(dicti: dict):
@@ -69,8 +75,7 @@ def show_result(dicti: dict):
         percent = {k: round(v * 100, 2) for k, v in value.items()}
         for j, v in enumerate(percent.values()):
             minus = 1 - ((j + 1) * 0.1)
-            axes[number].text(j - 0.5, minus, f"{round(v * 100, 2)}%", color='black')
-
+            axes[number].text(j - 0.5, minus, f"{round(v * 100, 2):.2f}%", color='black')
         axes[number].set_title(f"Bar - {key}")
         axes[number].grid(True)
 
@@ -89,6 +94,8 @@ def img_path_to_image(image_path, mode: PredictMode = PredictMode.NORMAL) -> Ima
     if mode == PredictMode.CANNY:
         image = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
         image = cv2.Canny(image, threshold1=170, threshold2=160)  # 155
+    if mode == PredictMode.ROTATION180:
+        image = cv2.rotate(image, 180)
 
     image = Image.fromarray(image)
     return image
@@ -98,17 +105,22 @@ def predict_mode(image, mode: PredictMode = PredictMode.NORMAL):
     scores = []
     print(image)
     path = ""
-    if mode is None or mode == PredictMode.NORMAL or mode == PredictMode.CANNY:
-        path = "dinov2/images/compare_items/ghs"  # todo problems with path depending on what location file is running
+    if mode is None or mode == PredictMode.NORMAL or mode == PredictMode.CANNY or mode == PredictMode.ROTATION180:
+        path = BASE_DIR / "images/compare_items/ghs"  # todo problems with path depending on what location file is running
     if mode == PredictMode.PHOTO:
-        path = "dinov2/images/compare_items/photos"  # "images/compare_items/photos"
+        path = BASE_DIR / "images/compare_items/photos"  # "images/compare_items/photos"
 
+    # print("PATH: ", path, "; BASE_DIR:", BASE_DIR)
     for compare_file in sorted(os.listdir(path=path)):
         compare_file = os.path.join(path, compare_file)
         # print(compare_file)
 
-        image_compare_file = img_path_to_image(compare_file, mode=mode)
-        image_image = img_path_to_image(image, mode=mode)
+        if mode == PredictMode.ROTATION180:
+            image_compare_file = img_path_to_image(compare_file, mode=mode)
+            image_image = img_path_to_image(image, mode=PredictMode.PHOTO)
+        else:
+            image_compare_file = img_path_to_image(compare_file, mode=mode)
+            image_image = img_path_to_image(image, mode=mode)
 
         similarity_score = compute_similarity(image_compare_file, image_image)
         scores.append(similarity_score)
@@ -125,11 +137,13 @@ def predict(image_path: str | np.ndarray):
     n_label, n_score = predict_mode(image_path, mode=PredictMode.NORMAL)
     p_label, p_score = predict_mode(image_path, mode=PredictMode.PHOTO)  # todo photo img data
     c_label, c_score = predict_mode(image_path, mode=PredictMode.CANNY)
+    r_label, r_score = predict_mode(image_path, mode=PredictMode.ROTATION180)
     print(n_label, n_score)
-    # print(p_label, p_score)
+    print(p_label, p_score)
     print(c_label, c_score)
+    print(r_label, r_score)
 
-    merged_scores = merge_scores([n_score, c_score])  #, p_score])
+    merged_scores = merge_scores([n_score, c_score, p_score])
     if n_label == c_label:  # == p_label
         print(n_label, merged_scores)  # todo
         d = {}
@@ -143,7 +157,7 @@ def predict(image_path: str | np.ndarray):
     return max_key, merged_scores
 
 
-def merge_scores(scores: list[dict]):
+def merge_scores(scores: list[dict], weights: list[np.float32] = None):
     scores_dict = {}
     labels = classes.copy()
     for label in labels:
@@ -176,4 +190,6 @@ def similarity(image1_path, image2_path, mode: PredictMode = PredictMode.NORMAL)
 
 
 if __name__ == '__main__':
-    print(predict("images/test/signal_ghs_test_13.jpeg"))
+    # print(predict("images/resources/ghs_test_image_exclamation_mark_rotated_180_degree.jpeg"))
+    print(predict(BASE_DIR / "images/resources/ghs_test_image_fire_rotated_180_degree.jpeg"))
+    # print(predict("images/test/signal_ghs_test_13.jpeg"))
